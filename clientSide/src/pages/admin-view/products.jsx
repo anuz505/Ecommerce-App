8e186a59f77dfa0e1d71f02a58e3d45ca6b7bdc5
anuj <anujb364@gmail.com>
2024-12-11 10:56:14 +0545

    added sheet

--- clientSide/src/pages/admin-view/products.jsx
+++ clientSide/src/pages/admin-view/products.jsx
@@ -1,8 +1,33 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { Fragment, useState } from "react";
 function AdminProducts() {
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
   return (
-    <div>
-      <h1>Admin Products</h1>
-    </div>
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => setOpenCreateProductsDialog(false)}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
   );
 }
 export default AdminProducts;
